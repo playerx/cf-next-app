ae081544e2a7323ad014d594aefeb7b3fcdcf20a
Ezeki Zibzibadze <ez@jok.io>
2023-01-06 17:22:31 +0400

    feat: add about page

---
+++ pages/about.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export const getServerSideProps = async () => {
+  console.log("API_KEY", process.env.API_KEY);
+
+  return {
+    props: {
+      hasApiKey: !!process.env.API_KEY,
+    },
+  };
+};
+
+export default function About({ hasApiKey }: { hasApiKey: boolean }) {
+  return (
+    <>
+      <Head>
+        <title>About Page {hasApiKey}</title>
+      </Head>
+
+      <main className={styles.main}>About Page</main>
+    </>
+  );
+}
